babd6990d1cedfc8c9ea5bdb337054ef2ff3906c
dreymotoh <striclyspoken@gmail.com>
2024-04-12 19:41:00 -0400

    Add project files.

---
+++ rock_papers_scissors/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace rock_papers_scissors
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            bool playAgain = true;
+            String player;
+            String computer;
+
+            while (playAgain)
+            {
+                Console.Write("Enter ROCK, PAPER, or SCISSORS: ");
+                player = Console.ReadLine();
+                player = player.ToLower();
+
+                Console.WriteLine(player);
+            }
+
+            Console.ReadKey();
+
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
