a8592c96708062c40b318c0ba3bfc164170e6412
dreymotoh <striclyspoken@gmail.com>
2024-04-12 20:40:18 -0400

    added switch statements for playing rock paper scissors in the terminal :)

--- rock_papers_scissors/Program.cs
+++ rock_papers_scissors/Program.cs
@@ -17,10 +17,64 @@
                 while (player != "ROCK" && player != "PAPER" && player != "SCISSORS") {
                     Console.Write("Enter ROCK, PAPER, or SCISSORS: ");
                     player = Console.ReadLine();
-                    player = player.ToLower();
+                    player = player.ToUpper();
 
-                    Console.WriteLine(player);
+                    switch (random.Next(1, 4)) {
+                        case 1:
+                            computer = "ROCK";
+                            break;
+                        case 2:
+                            computer = "PAPER";
+                            break;
+                        case 3:
+                            computer = "SCISSORS";
+                            break;
+                    }
+                    Console.WriteLine("Player: " + player);
+                    Console.WriteLine("Computer: " + computer);
+
+                    switch (player) 
+                    {
+                        case "ROCK":
+                            if (computer == "ROCK") {
+                                Console.WriteLine("Its a draw!");
+                            } else if (computer == "PAPER") {
+                                Console.WriteLine("You lose!");
+                            } else {
+                                Console.WriteLine("You Win!");
+                            }
+                            break;
+                        case "PAPER":
+                            if (computer == "ROCK")
+                            {
+                                Console.WriteLine("You Win!");
+                            }
+                            else if (computer == "PAPER")
+                            {
+                                Console.WriteLine("Its a draw!");
+                            }
+                            else
+                            {
+                                Console.WriteLine("You lose!");
+                            }
+                            break;
+                        case "SCISSORS":
+                            if (computer == "ROCK")
+                            {
+                                Console.WriteLine("You lose!");
+                            }
+                            else if (computer == "PAPER")
+                            {
+                                Console.WriteLine("You Win!");
+                            }
+                            else
+                            {
+                                Console.WriteLine("Its a draw!");
+                            }
+                            break;
+                    }
                 }
+                Console.ReadKey();
             }
 
             Console.ReadKey();
